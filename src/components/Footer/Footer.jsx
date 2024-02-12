a1e391b3ed3d7519777c4335e1e9597567a37ba7
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-02-12 23:02:14 +0300

    feat: footer, header, home

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,46 @@
-import React from 'react';
-import styles from './Footer.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { ROUTES } from '../../utils/routes';
-import logo from '../../images/logo.svg';
-function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.logo}>
-        <Link to={ROUTES.HOME}>
-          <img src={logo} alt="Stuff" />
-        </Link>
-      </div>{' '}
-      <div className={styles.rights}>
-        Developed by{' '}
-        <a
-          href="https://github.com/Anatoli1022?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Anatoli
-        </a>
-      </div>{' '}
-      <div className={styles.socials}>
-        <a
-          href="https://www.instagram.com/anatolik153/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <svg className="icon">
-            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
-          </svg>
-        </a>
+import styles from "../../styles/Footer.module.css";
+import { ROUTES } from "../../utils/routes";
 
-        {/* <a href="https://facebook.com" target="_blank" rel="noreferrer">
-          <svg className="icon">
-            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
-          </svg>
-        </a>
+import LOGO from "../../images/logo.svg";
 
-        <a href="https://youtube.com" target="_blank" rel="noreferrer">
-          <svg className="icon">
-            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
-          </svg>
-        </a> */}
-      </div>
-    </footer>
-  );
-}
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={LOGO} alt="Stuff" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>
+      Developed by{" "}
+      <a href="https://youtube.com/Tomkovich" target="_blank" rel="noreferrer">
+        Tomkovich
+      </a>
+    </div>
+
+    <div className={styles.socials}>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+        </svg>
+      </a>
+
+      <a href="https://facebook.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+        </svg>
+      </a>
+
+      <a href="https://youtube.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+        </svg>
+      </a>
+    </div>
+  </section>
+);
 
 export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import Poster from '../Poster/Poster';
-import Products from '../Products/Products';
-import Categories from '../Categories/Categories';
-import { useDispatch, useSelector } from 'react-redux';
-import Banner from '../Banner/Banner';
-import { filterByPrice } from '../../features/products/productsSlice';
+import { filterByPrice } from "../../features/products/productsSlice";
+
+import Banner from "../Banner/Banner";
+import Categories from "../Categories/Categories";
+import Poster from "../Poster/Poster";
+import Products from "../Products/Products";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -15,17 +16,17 @@ const Home = () => {
   } = useSelector((state) => state);
 
   useEffect(() => {
-    if (!list.length) {
-      return;
-    }
+    if (!list.length) return;
+
     dispatch(filterByPrice(100));
   }, [dispatch, list.length]);
+
   return (
     <>
       <Poster />
       <Products products={list} amount={5} title="Trending" />
       <Categories products={categories.list} amount={5} title="Worth seeing" />
-      <Banner />{' '}
+      <Banner />
       <Products products={filtered} amount={5} title="Less than 100$" />
     </>
   );
